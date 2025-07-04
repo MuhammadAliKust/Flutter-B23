6bf082b16cf8040aaa9f444225dfa732db9794c5
Muhammad Ali <kustalics@gmail.com>
2025-07-04 16:13:53 +0500

    Background Image Added

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -7,24 +7,33 @@ class ListViewDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("List View Demo")),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, i) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              elevation: 10,
-              color: Colors.blue,
-              child: ListTile(
-                leading: Icon(Icons.notifications),
-                title: Text("Notification Title"),
-                subtitle: Text("Notification Sub Title"),
-                trailing: Icon(Icons.arrow_forward),
-                // tileColor: Colors.blue,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, i) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 10,
+                color: Colors.blue,
+                child: ListTile(
+                  leading: Icon(Icons.notifications),
+                  title: Text("Notification Title"),
+                  subtitle: Text("Notification Sub Title"),
+                  trailing: Icon(Icons.arrow_forward),
+                  // tileColor: Colors.blue,
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
