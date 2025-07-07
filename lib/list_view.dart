68a72838fb1f06e290b163ce324f17396878fa02
Muhammad Ali <kustalics@gmail.com>
2025-07-07 15:57:37 +0500

    Dynamic List View Done

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -1,7 +1,22 @@
+import 'package:fltter_b23/models/notification.dart';
 import 'package:flutter/material.dart';
 
 class ListViewDemo extends StatelessWidget {
-  const ListViewDemo({super.key});
+  ListViewDemo({super.key});
+
+  List<NotificationModel> myList = [
+    NotificationModel(
+      title: 'First Notification',
+      description: 'First Description',
+      time: '11:45',
+    ),
+    NotificationModel(
+      title: 'Second Notification',
+      description: 'Second Description',
+      time: '1:45',
+    ),
+    // NotificationModel(title: 'Third Notification', description: 'Third Description', time: '12:45'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,26 +24,24 @@ class ListViewDemo extends StatelessWidget {
       appBar: AppBar(title: Text("List View Demo")),
       body: Container(
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/1.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
+        // decoration: BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage('assets/images/1.jpg'),
+        //     fit: BoxFit.cover,
+        //   ),
+        // ),
         child: ListView.builder(
-          itemCount: 10,
+          itemCount: myList.length,
           itemBuilder: (context, i) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Card(
-                elevation: 10,
                 color: Colors.blue,
                 child: ListTile(
                   leading: Icon(Icons.notifications),
-                  title: Text("Notification Title"),
-                  subtitle: Text("Notification Sub Title"),
-                  trailing: Icon(Icons.arrow_forward),
-                  // tileColor: Colors.blue,
+                  title: Text(myList[i].title),
+                  subtitle: Text(myList[i].description),
+                  trailing: Text(myList[i].time),
                 ),
               ),
             );
